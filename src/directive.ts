c4b340086f3589fa47a2aee5f2f28a92b6cc2da9
M Ekramy <m.ajamekramy@gmail.com>
2021-10-10 11:24:40 +0330

    optimize directive core

--- src/directive.ts
+++ src/directive.ts
@@ -1,37 +1,19 @@
 import { ObjectDirective } from "@vue/runtime-core";
 import { createTextMaskInputElement } from "text-mask-core";
-import { Mask, parseMask } from "./options";
+import { parseMask } from "./options";
 
-interface MaskRecord {
-    instance: any;
-    mask: Mask;
-    options: Record<string, unknown>;
-}
-const records = new Map<HTMLInputElement, MaskRecord>();
-
-const getInput = (el: any): HTMLInputElement =>
-    el instanceof HTMLInputElement ? el : el.querySelector("input") || el;
-const getOptions = (v: any) => {
-    const def = { mask: "", options: { guide: false } };
-    const options = v.mask ? v : { mask: v };
-    options.mask = parseMask(options.mask);
-    return Object.assign(def, options);
-};
-
-const setRecord = (
-    el: HTMLInputElement,
-    options: any
-): MaskRecord | undefined => {
-    records.set(el, {
-        instance: createTextMaskInputElement({
-            inputElement: el,
-            mask: options.mask,
-            ...(options.options || {})
-        }),
-        mask: options.mask,
-        options: options.options
+const records = new Map<HTMLInputElement, any>();
+const attachMasker = (el: HTMLInputElement, options: any): any => {
+    const { parseOptions } = useUtils();
+    const _options = parseOptions(options);
+    const instance = createTextMaskInputElement({
+        inputElement: el,
+        mask: _options.mask,
+        ...(_options.options || {})
     });
-    return records.get(el);
+    records.set(el, instance);
+    instance.update();
+    return instance;
 };
 
 /**
@@ -39,28 +21,42 @@ const setRecord = (
  */
 export const vMask: ObjectDirective<any> = {
     mounted(el, { value }) {
-        const iEl = getInput(el);
-        const options = getOptions(value);
-        const rec = setRecord(iEl, options);
-        rec && rec.instance && rec.instance.update();
+        const { findInput } = useUtils();
+        const _el = findInput(el);
+        _el && attachMasker(_el, value);
     },
     updated(el, { oldValue, value }) {
-        const iEl = getInput(el);
-        let record: MaskRecord | undefined = undefined;
-        const oldOptions = getOptions(oldValue);
-        const newOptions = getOptions(value);
-        if (JSON.stringify(oldOptions) != JSON.stringify(newOptions)) {
-            record = setRecord(iEl, newOptions);
+        const { findInput } = useUtils();
+        const _el = findInput(el);
+        if (!_el) return;
+        if (JSON.stringify(oldValue) !== JSON.stringify(value)) {
+            attachMasker(_el, value);
         } else {
-            record = records.get(iEl);
+            const instance = records.get(_el);
+            instance && instance.update();
         }
-        record && record.instance && record.instance.update();
-        iEl.dispatchEvent(
+        _el.dispatchEvent(
             new Event("input", { bubbles: true, cancelable: true })
         );
     },
     beforeUnmount(el) {
-        const iEl = getInput(el);
-        records.delete(iEl);
+        const { findInput } = useUtils();
+        const _el = findInput(el);
+        _el && records.delete(_el);
     }
 };
+
+function useUtils() {
+    function findInput(el: any): HTMLInputElement | null {
+        return el instanceof HTMLInputElement
+            ? el
+            : el.querySelector("input") || null;
+    }
+    function parseOptions(v: any) {
+        const def = { mask: "", options: { guide: false } };
+        const options = v.mask ? v : { mask: v };
+        options.mask = parseMask(options.mask);
+        return Object.assign(def, options);
+    }
+    return { findInput, parseOptions };
+}
